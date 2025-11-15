9374045c605cc6f5eb679653e553b9f6a76ff747
ChrisM2309 <chris.marroquin2309@gmail.com>
2025-11-14 21:50:14 -0600

    Base de Datos conectada. Archivo de Test, para probar leer eventos
    - Database conectada con CDN
    - Archivo test, temporal, solo es para probar conexion con la base

---
+++ js/firebase_config.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+//import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCLNj65uwgORStzqR7FulNIuDT7lqnaE5s",
+    authDomain: "landing-c3.firebaseapp.com",
+    projectId: "landing-c3",
+    storageBucket: "landing-c3.firebasestorage.app",
+    messagingSenderId: "911691009600",
+    appId: "1:911691009600:web:e8c1433f78823634b0d9a5",
+    measurementId: "G-Z2JB5KBFZK"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//export const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export {app, db}; 
